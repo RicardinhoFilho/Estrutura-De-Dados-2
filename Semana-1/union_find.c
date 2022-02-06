30b2d1c9408b0c8a51d7a5242e1ea7ebb7af0f74
Ricardo Filho <ricardinhogiasson16@gmail.com>
2022-02-06 00:05:40 -0300

    Finalizando Atividade Semana 1

--- Semana-1/union_find.c
+++ Semana-1/union_find.c
@@ -24,7 +24,7 @@ UF *init_UF(int n)
     uf->id = malloc(sizeof(int) * n);
     uf->n = n;
     uf->count = n;
-    for (i = 0; i < uf->count; ++i)
+    for (i = 0; i <= uf->count; ++i)
     {
         uf->id[i] = i;
     }
@@ -39,42 +39,25 @@ int count_UF(UF *uf)
 
 int connected_UF(UF *uf, int p, int q)
 {
-
-    return (find_UF(uf, p) == find_UF(uf, q));
+    
+    printf("%d == %d\n",find_UF(uf, p) , find_UF(uf, q));
+ return find_UF(uf, p) == find_UF(uf, q)?  1 : 0;
 }
 
 int find_UF(UF *uf, int p)
 {
-    return (uf->id[p]);
+   return(uf->id[p]);
+   
 }
 
 void union_UF(UF *uf, int p, int q)
 {
-    if (!uf->id[p])
-    {
-        uf->id[p] = p;
-    }
-
-    if (!uf->id[q])
-    {
-        uf->id[q] = uf->id[p];
-    }
-    else if (uf->id[q])
-    {
-        for (int i = 0; i < uf->count; ++i)
-        {
-            //printf("Comparando %d e %d\n", uf->id[q] , uf->id[i]);
-
-            if (uf->id[q] == uf->id[i])
-            {
-                uf->id[q] = uf->id[p];
-                //printf("Conectando %d à o grupo %d!\n", i, uf->id[p]);
-            }
-        }
-    }
-
-    printf("%d e %d estão foram conectados com o ID: %d\n", p, q, uf->id[p]);
-
+    int aux = find_UF(uf,p);
+   for (int i = 0; i < uf->n; i++)
+      if (find_UF(uf,i) == aux) {
+               printf("CONECTANDO o %dcom o %d\n\n ", i, uf->id[q]);
+          uf->id[i] = uf->id[q];};
+   uf->count--;
     return;
 }
 
@@ -90,13 +73,16 @@ int main()
     scanf("%d %d", &p, &q);
     while (p > -1 && q > -1)
     {
-        //printf("%d %d\n", p, q);
+        
         if (!connected_UF(uf, p, q))
         {
-            printf("p = %d q = %d\n", p, q);
+            printf("%d %d\n", p, q);
             union_UF(uf, p, q);
+        }else{
+            printf("CONECTADOS");
         }
 
         scanf("%d %d", &p, &q);
     }
-}
+    printf("%d Componentes",count_UF(uf));
+}   
